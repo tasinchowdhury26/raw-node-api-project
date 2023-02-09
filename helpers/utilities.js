d9cfa7b81b5b25506b63eccf272d1b62b6e389e8
tasinchowdhury26 <tasinchowdhury26@gmail.com>
2023-02-09 16:01:27 +0600

    added token functionalities to the user methods

--- helpers/utilities.js
+++ helpers/utilities.js
@@ -1,3 +1,4 @@
+/* eslint-disable prefer-const */
 const crypto = require('crypto');
 const environments = require('./environments');
 
@@ -19,6 +20,25 @@ utilities.hash = (str) => {
     if (typeof str === 'string' && str.length > 0) {
         const hash = crypto.createHmac('sha256', environments.secretKey).update(str).digest('hex');
         return hash;
+    }
+    return false;
+};
+
+// create random string
+utilities.createRandomString = (strlength) => {
+    let length = strlength;
+    length = typeof strlength === 'number' && strlength > 0 ? strlength : false;
+
+    if (length) {
+        const possiblecharacters = 'abcdefghijklmnopqrstuvwxyz1234567890';
+        let output = '';
+        for (let i = 1; i <= length; i += 1) {
+            const randomCharacter = possiblecharacters.charAt(
+                Math.floor(Math.random() * possiblecharacters.length),
+            );
+            output += randomCharacter;
+        }
+        return output;
     }
     return false;
 };
